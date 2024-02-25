db5305346869356b565b39575c4aab1ebd32a4c1
Patrick <patricktran2k5@gmail.com>
2024-02-25 20:37:51 +0700

    Ver 1.0.0

--- TicTacToeGame/assets/js/main.js
+++ TicTacToeGame/assets/js/main.js
@@ -1,6 +1,6 @@
-const cells = document.querySelectorAll('.cell');
-const statusText = document.querySelector('#status-text');
-const restartBtn = document.querySelector('#restart-btn');
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#status-text");
+const restartBtn = document.querySelector("#restart-btn");
 const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -11,26 +11,70 @@ const winConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-
-let options = ["","","","","","","","",""];
+let options = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let running = false;
 
-function initializeGame() {
-    
+initializeGame();
+
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
 }
-function cellClicked() {
-    
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
 }
-function updataCell(cell, index) {
-    
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
 }
-function changePlayer() {
-    
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
 }
-function checkWinner() {
-    
+function checkWinner(){
+    let roundWon = false;
+
+    for(let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
 }
-function restartGame() {
-    
+function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
 }
